d6ef5f76b16f54380e39d00f19cbbd1a6b44a2c7
root <root@journals-stage.oslomet.no>
2021-03-05 23:05:56 +0100

    add theme for professions and professionalism

---
+++ professionsprofessionalism/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @defgroup plugins_themes_professionsprofessionalism ProfessionsProfessionalism theme plugin
+ */
+ 
+/**
+ * @file plugins/themes/professionsprofessionalism/index.php
+ *
+ * Copyright (c) 2014-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_themes_professionsprofessionalism
+ * @brief Wrapper for professionsprofessionalism theme plugin.
+ *
+ */
+
+require_once('ProfessionsProfessionalismThemePlugin.inc.php');
+
+return new ProfessionsProfessionalismThemePlugin();
+
+
