56df7fec4cf8e54e6f7189aa27a24d466af5d7a4
root <root@journals.oslomet.no>
2021-09-10 14:16:41 +0200

    add un-customized nbf theme

---
+++ nbf/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @defgroup plugins_themes_nbf nbf theme plugin
+ */
+ 
+/**
+ * @file plugins/themes/nbf/index.php
+ *
+ * Copyright (c) 2014-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_themes_nbf
+ * @brief Wrapper for nbf theme plugin.
+ *
+ */
+
+require_once('nbfThemePlugin.inc.php');
+
+return new nbfThemePlugin();
+
+
