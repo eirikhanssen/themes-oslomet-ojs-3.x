e43a5882f9f3ed16e067fb28b31393bd357418af
Eirik Hanssen <eirikhanssen79@gmail.com>
2021-10-11 14:28:55 +0200

    add information theme

---
+++ information/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @defgroup plugins_themes_information Information theme plugin
+ */
+ 
+/**
+ * @file plugins/themes/information/index.php
+ *
+ * Copyright (c) 2014-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_themes_information
+ * @brief Wrapper for information theme plugin.
+ *
+ */
+
+require_once('InformationThemePlugin.inc.php');
+
+return new InformationThemePlugin();
+
+
