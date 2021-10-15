887c78e680b11632ec949de548a5227a3b8333d9
Eirik Hanssen <eirikhanssen79@gmail.com>
2021-10-15 17:04:14 +0200

    add Radiography Open theme

---
+++ radiographyopen/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @defgroup plugins_themes_radiographyopen RadiographyOpen theme plugin
+ */
+ 
+/**
+ * @file plugins/themes/radiographyopen/index.php
+ *
+ * Copyright (c) 2014-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_themes_radiographyopen
+ * @brief Wrapper for radiographyopen theme plugin.
+ *
+ */
+
+require_once('RadiographyOpenThemePlugin.inc.php');
+
+return new RadiographyOpenThemePlugin();
+
+
