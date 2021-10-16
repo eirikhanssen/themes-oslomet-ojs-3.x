2d2768338184736728f4a6a502a7e3352bec49b7
Eirik Hanssen <eirikhanssen79@gmail.com>
2021-10-16 20:16:08 +0200

    add new theme technea, fix css specificity mistake to select header button fg color

---
+++ technea/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @defgroup plugins_themes_technea TechneA theme plugin
+ */
+ 
+/**
+ * @file plugins/themes/technea/index.php
+ *
+ * Copyright (c) 2014-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_themes_technea
+ * @brief Wrapper for technea theme plugin.
+ *
+ */
+
+require_once('TechneAThemePlugin.inc.php');
+
+return new TechneAThemePlugin();
+
+
